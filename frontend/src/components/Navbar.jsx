d52aadd02dff2a17a802f536d736b0c91c824b00
TensaiMT <miarotianaraj@gmail.com>
2024-03-04 18:46:13 +0300

    Navbar

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>About us</li>
+        <li>Food</li>
+        <li>Menu</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import Navbar from '../components/Navbar';
+import './Home.css'
+
+function Home() {
+  return (
+    <>
+      <Navbar>
+        
+      </Navbar>
+    </>
+  );
+}
+
+export default Home;
